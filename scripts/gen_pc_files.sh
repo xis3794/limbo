8ac40f5cfb693a02dc64147bcdcede379a22da43
xis3794 <xis3794@users.noreply.github.com>
2026-08-14 11:08:33 +0000

    Fix: add libslirp build dir to slirp.pc Cflags (libslirp-version.h generated there)

--- scripts/gen_pc_files.sh
+++ scripts/gen_pc_files.sh
@@ -71,12 +71,13 @@ prefix=$JNI/libslirp
 exec_prefix=\${prefix}
 libdir=$OBJ
 includedir=\${prefix}/src
+buildincludedir=\${prefix}/build
 
 Name: slirp
 Description: libslirp (Limbo CI-built)
 Version: 4.7.0
 Libs: -L\${libdir} -lslirp
-Cflags: -I\${includedir}
+Cflags: -I\${includedir} -I\${buildincludedir}
 EOF
 
 echo "[OK] pkg-config files generated in $PC:"
