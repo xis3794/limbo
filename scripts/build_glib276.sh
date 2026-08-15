f644798a6ec8b1e409df699241d1123ea530bc64
xis3794 <xis3794@users.noreply.github.com>
2026-08-15 08:34:08 +0000

    Fix glib branch: use make ifeq (not shell exit0 which still ran 2.56 flow); glib c_link_args for iconv

--- scripts/build_glib276.sh
+++ scripts/build_glib276.sh
@@ -56,6 +56,7 @@ cd "$GLIB_SRC"
 meson setup "$GLIB_BUILD" . --cross-file "$JNI/glib-cross.txt" \
   --default-library=shared -Dprefix=/usr \
   -Dc_args="-I$JNI/compat/musl/include -Wno-unknown-warning-option" \
+  -Dc_link_args="-L$OBJ -lcompat-musl -llog -Wl,-z,undefs" \
   -Dlibmount=disabled -Dselinux=disabled -Ddtrace=false \
   -Dsystemtap=false -Dgtk_doc=false -Dman=false \
   -Dtests=false -Dinstalled_tests=false \
