41e8065ef048b137a31d7fb3b01b9992a9f20f61
xis3794 <xis3794@users.noreply.github.com>
2026-08-17 06:28:37 +0000

    libintl: version-script exports _rwlock_* under LIBC version - glib meson-linking binds verdef names (_rwlock_trywrlock@LIBC), Huawei needs versioned-def match

--- limbo-android-lib/src/main/jni/compat/intl/Android.mk
+++ limbo-android-lib/src/main/jni/compat/intl/Android.mk
@@ -7,4 +7,8 @@ LOCAL_SRC_FILES := intl_stubs.c
 LOCAL_MODULE := intl
 
 LOCAL_ARM_MODE := $(ARM_MODE)
+# version-script: export _rwlock_* etc. under the LIBC version name so
+# glib's _rwlock_trywrlock@LIBC references resolve (Huawei linker requires
+# versioned-ref -> versioned-def matching)
+LOCAL_LDFLAGS += -Wl,--version-script=$(LOCAL_PATH)/intl.map
 include $(BUILD_SHARED_LIBRARY)
